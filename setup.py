391fb3f51e0cb40357f5e3240fbeded0416ec138
Luke Funk <funkl@funkelab-ws3.int.janelia.org>
2022-01-10 17:36:09 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='deep_dish',
+    version='0.1',
+    url='https://github.com/lukebfunk/deep_dish',
+    packages=[
+        'deep_dish',
+        ])
